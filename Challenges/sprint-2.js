8d0a20770a8c7e70aed6fbb1ececd12ea83405c7
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2023-04-22 00:10:08 -0300

    sprint-2
    This commit contains the code for a product class in an e-commerce database. The class is designed to manage products in the database and has five main methods for managing product data.

--- Challenges/sprint-2.js
+++ Challenges/sprint-2.js
@@ -2,39 +2,10 @@
 /////////////////////////////
 // MANEJO DE ARCHIVOS
 /////////////////////////////
-Consigna
-
 - Modificar la clase ProductManager para que gestione un conjunto de productos de un archivo. Empezar a construir la base de datos (en un archivo)
 
-- La clase debe recibir como parámetro la ruta donde se creará el archivo y el constructor debe incluir esta ruta en la variable this.path
-
-- Cada producto que gestione debe contar con las propiedades ya vistas en la tarea anterior.
-
-- addProduct agrega un producto al arreglo de productos del archivo. Todos los campos son obligatorios menos:
-> id que debe agregarse automáticamente y autoincrementable
-> stock es opcional ya que si el usuario no lo envía debe ser cero
-> En caso de éxito devolver el id del producto
-> En caso de error devolver un mensaje que diga: “addProduct: error”
-
-- getProducts debe devolver el arreglo con todos los productos guardados en el archivo.
-> En caso de que no haya productos devolver: “Not found”
-> En caso de error devolver un mensaje que diga: “getProducts: error”
-
-- getProductById(id) debe recibir como parámetro el id del producto y debe devolver un objeto con todas las propiedades del producto.
-> En caso de no coincidir devolver: “Not found”
-> En caso de error devolver un mensaje que diga: “getProductById: error”
-
-- updateProduct(id,data) debe recibir un id y un objeto data con las propiedades a modificar del producto.
-> En caso de no coincidir devolver: “Not found”
-> En caso de éxito devolver un mensaje que diga:  “updateProduct: done”
-> En caso de error devolver un mensaje que diga: “updateProduct: error”
-
-- deleteProduct(id)debe recibir como parámetro el id del producto y debe borrar el producto del archivo.
-> En caso de no coincidir devolver: “Not found”
-> En caso de éxito devolver un mensaje que diga:  “deleteProduct: done”
-> En caso de error devolver un mensaje que diga: “deleteProduct: error”
+- La clase debe recibir como parámetro la ruta donde se creará el archivo y el constructor debe incluir esta ruta en la variable this.path. Cada producto que gestione debe contar con las propiedades ya vistas en la tarea anterior.
 */
-
 const fs = require("fs");
 
 class ProductManager {
@@ -57,33 +28,254 @@ class ProductManager {
 			try {
 				const data = fs.readFileSync(path, "utf-8");
 				this.products = JSON.parse(data);
-
-    console.log('this.products INIT ', this.products)
 			} catch (err) {
 				console.log("There was a problem reading the file");
 			}
 		}
 	}
 
-	addProduct() {
-		try {
-			const new_product = {
-				product: "Product",
-			};
-			console.log("this.products ", this.products);
-			this.products.push(new_product);
-			console.log("products ", this.products);
-			let products = this.products;
+	async addProduct({ title, description, price, thumbnail, stock }) {
+		// - addProduct agrega un producto al arreglo de productos del archivo.
+		// Todos los campos son obligatorios menos:
+		// > id que debe agregarse automáticamente y autoincrementable
+		// > stock es opcional ya que si el usuario no lo envía debe ser cero
+		// > En caso de éxito devolver el id del producto
+		// > En caso de error devolver un mensaje que diga: “addProduct: error”
+		let repeatedIndex = this.products.findIndex(
+			(product) => product.title === title
+		);
 
-			fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+		if (repeatedIndex == !-1) {
+			return console.log(
+				`This product ${title} already exists at index ${repeatedIndex}`
+			);
+		}
+
+		let id;
+
+		if (this.products.length === 0) {
+			id = 1;
+		} else {
+			let lastProduct = this.products[this.products.length - 1];
+			id = lastProduct.id + 1;
+		}
+
+		const new_product = {
+			id,
+			title,
+			description,
+			price,
+			thumbnail,
+			stock: stock ?? 10,
+		};
+
+		try {
+			this.products.push(new_product);
+
+			await fs.promises.writeFile(
+				this.path,
+				JSON.stringify(this.products, null, 2)
+			);
+
+			return new_product.id;
 		} catch (err) {
-			console.log("There was a problem creating a new product");
-			console.log(err);
+			return "addProduct: error";
+		}
+	}
+
+	async getProducts(quantity) {
+		// getProducts debe devolver el arreglo con todos los productos guardados en el archivo y en caso de que no haya productos devolver: “Not found”
+		// En caso de error devolver un mensaje que diga: “getProducts: error”
+		try {
+			if (this.products.length === 0) {
+				return console.log("Not found");
+			}
+			quantity = quantity ?? 5;
+			return console.log(this.products.slice(0, quantity));
+		} catch (error) {
+			return console.log("getProducts: error");
+		}
+	}
+
+	async getProductById(id) {
+		// - getProductById(id) debe recibir como parámetro el id del producto y debe devolver un objeto con todas las propiedades del producto.
+		// > En caso de no coincidir devolver: “Not found”
+		// > En caso de error devolver un mensaje que diga: “getProductById: error”
+		try {
+			const product = this.products.find((product) => product.id === id);
+
+			if (product) {
+				console.log(product)
+				return product;
+			} else {
+				return console.log("Not found");
+			}
+		} catch (error) {
+			return console.log("getProductById: error");
+		}
+	}
+
+	async updateProduct(id, data) {
+		// - updateProduct(id,data) debe recibir un id y un objeto data con las propiedades a modificar del producto.
+		// > En caso de no coincidir devolver: “Not found”
+		// > En caso de éxito devolver un mensaje que diga:  “updateProduct: done”
+		// > En caso de error devolver un mensaje que diga: “updateProduct: error”
+		try {
+			let product = await this.getProductById(id);
+
+			if (Object.keys(data).length === 0) {
+				return console.log("No data to update");
+			}
+
+			for (const prop in data) {
+				product[prop] = data[prop];
+			}
+
+			await fs.promises.writeFile(
+				this.path,
+				JSON.stringify(this.products, null, 2)
+			);
+
+			return console.log("updateProduct: done");
+		} catch (error) {
+			return console.log("updateProduct: error");
+		}
+	}
+
+	async deleteProduct(id) {
+		// - deleteProduct(id)debe recibir como parámetro el id del producto y debe borrar el producto del archivo.
+		// > En caso de no coincidir devolver: “Not found”
+		// > En caso de éxito devolver un mensaje que diga:  “deleteProduct: done”
+		// > En caso de error devolver un mensaje que diga: “deleteProduct: error”
+		try {
+			let product = await this.getProductById(id);
+
+			if (!product) {
+				return console.log("Not found");
+			}
+			this.products = this.products.filter((product) => product.id !== id);
+
+			await fs.promises.writeFile(
+				this.path,
+				JSON.stringify(this.products, null, 2)
+			);
+			
+			return console.log("deleteProduct: done");
+		} catch (error) {
+			return console.log("deleteProduct: error");
 		}
 	}
 }
 
+/////////////////////////////
+// DEFINE ProductManager INSTANCE
 let product_manager = new ProductManager("./data/products.json");
 
-product_manager.addProduct();
+/////////////////////////////
+// ADD PRODUCTS
+console.log("PRODUCTS ADDED TO products.json FILE");
+/* 
+product_manager.addProduct({
+	title: "Echeveria Succulent",
+	description: "Beautiful pink and green rosette-shaped succulent. Easy to care for and perfect for any indoor or outdoor space.",
+	price: 15,
+	thumbnail: "https://example.com/echeveria.jpg",
+	stock: 8,
+});
 
+product_manager.addProduct({
+	title: "Snake Plant",
+	description: "Stylish and low-maintenance, the snake plant is perfect for any home. Its long, upright leaves add a modern touch to any room.",
+	price: 20,
+	thumbnail: "https://example.com/snake_plant.jpg",
+	stock: 10,
+});
+
+product_manager.addProduct({
+	title: "ZZ Plant",
+	description: "The ZZ plant is a low-light champion, making it perfect for offices and homes alike. Its glossy leaves add a touch of sophistication to any space.",
+	price: 18,
+	thumbnail: "https://example.com/zz_plant.jpg",
+	stock: 5,
+});
+
+product_manager.addProduct({
+	title: "Mint Herb Plant",
+	description: "Fresh and fragrant, this herb is perfect for adding to drinks, desserts, or as a garnish. Easy to grow and perfect for any kitchen.",
+	price: 12,
+	thumbnail: "https://example.com/mint_plant.jpg",
+});
+
+product_manager.addProduct({
+	title: "Fiddle Leaf Fig",
+	description: "A popular indoor tree with large, violin-shaped leaves. Its dramatic silhouette adds a touch of elegance to any room.",
+	price: 45,
+	thumbnail: "https://example.com/fiddle_leaf_fig.jpg",
+	stock: 2,
+});
+
+product_manager.addProduct({
+	title: "Aloe Vera Plant",
+	description: "This succulent is not only easy to care for, but its gel can also soothe burns and other skin irritations. A must-have for any home.",
+	price: 17,
+	thumbnail: "https://example.com/aloe_vera.jpg",
+	stock: 6,
+});
+
+product_manager.addProduct({
+	title: "Lavender Plant",
+	description: "Known for its calming aroma, lavender is a versatile herb that can be used in cooking or for aromatherapy. Easy to care for and beautiful to look at.",
+	price: 14,
+	thumbnail: "https://example.com/lavender_plant.jpg",
+});
+
+product_manager.addProduct({
+	title: "Spider Plant",
+	description: "One of the easiest plants to care for, the spider plant is perfect for beginners. Its long, spindly leaves make it a great addition to any room.",
+	price: 10,
+	thumbnail: "https://example.com/spider_plant.jpg",
+	stock: 7,
+});
+
+product_manager.addProduct({
+	title: "String of Pearls",
+	description: "This unique succulent has long, thin stems with tiny green 'pearls' that trail down. Perfect for hanging baskets or as a statement piece.",
+	price: 22,
+	thumbnail: "https://example.com/string_of_pearls.jpg",
+	stock: 4,
+});
+
+product_manager.addProduct({
+	title: "Basil Herb Plant",
+	description: "A staple herb in many kitchens, basil is easy to grow and adds a fresh, aromatic flavor to any dish.",
+	price: 9,
+	thumbnail: "https://example.com/basil_plant.jpg",
+});
+*/
+
+/////////////////////////////
+// GET PRODUCT BY ID
+console.log("GET PRODUCT BY ID");
+// product_manager.getProductById(1);
+
+/////////////////////////////
+// UPDATE PRODUCT
+console.log("UPDATE PRODUCT");
+// product_manager.updateProduct(9, {
+//  "title": "Thread of Silofornias",
+// "description": "This unique succulent has long, thin stems with tiny green 'pearls' that trail down. Perfect for hanging baskets or as a statement piece.",
+// "price": 22,
+// "thumbnail": "https://example.com/string_of_pearls.jpg",
+// "stock": 4
+// });
+
+/////////////////////////////
+// DELETE PRODUCT
+console.log("DELETE PRODUCT");
+// product_manager.deleteProduct(10);
+
+/////////////////////////////
+// GET PRODUCTS
+console.log("GET PRODUCTS");
+// Insert quantity, default 5
+product_manager.getProducts();
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Codehouse_Backend
- This GitHub repository is a collection of code examples and exercises used in the backend full course offered by Coderhouse. The course covers a variety of topics related to backend development, including building RESTful APIs, working with databases, and deploying applications.
+# E-Commerce Product Class Repository
+
+## Introduction
+This repository contains the code for a product class in an e-commerce database. The class is designed to manage products in the database and has five main methods for managing product data.
+
+## Methods
+
+### `addProduct(product)`
+The `addProduct()` method is used to add a new product to the database. It takes a product object as an argument and adds it to the database.
+
+### `getProducts()`
+The `getProducts()` method is used to retrieve all the products that have been created and stored in the database. It returns an array of all the products in the database.
+
+### `getProductById(id)`
+The `getProductById()` method is used to retrieve a specific product from the database. It takes an id as an argument and returns the product that matches that id.
+
+### `updateProduct(id, product)`
+The `updateProduct()` method is used to update a specific product in the database. It takes an id and a product object as arguments and updates the product that matches that id.
+
+### `deleteProduct(id)`
+The `deleteProduct()` method is used to delete a specific product from the database. It takes an id as an argument and deletes the product that matches that id.
+
+## Usage
+To use this class, simply import it into your project and create a new instance of the product class. Then, you can call any of the five methods to manage product data. The `addProduct()` method will add a new product to the database, the `getProducts()` method will retrieve all products in the database, the `getProductById()` method will retrieve a specific product from the database, the `updateProduct()` method will update a specific product in the database, and the `deleteProduct()` method will delete a specific product from the database.
+
+Now all data is saved in a file that will be created if not exists using fs ile system module, in a file called "products.json" in a folder called "data". And all calls and data is updated and managed using trycatch blocks also using fs.promises and their methods.
