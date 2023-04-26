dc8fdbd30517bd422d5112823c3345bf8ded5417
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2023-04-26 20:15:19 -0300

    Improved Error Handling and Exception Throwing in Class Methods
    Several return console.log() statements have been replaced with exceptions to provide more detailed error messages to the user. Exceptions have been added to methods getProductById, getProducts, updateProduct, and deleteProduct.

--- Challenges/sprint-2.js
+++ Challenges/sprint-2.js
@@ -45,7 +45,7 @@ class ProductManager {
 			(product) => product.title === title
 		);
 
-		if (repeatedIndex == !-1) {
+		if (repeatedIndex !== -1) {
 			return console.log(
 				`This product ${title} already exists at index ${repeatedIndex}`
 			);
@@ -79,7 +79,8 @@ class ProductManager {
 
 			return new_product.id;
 		} catch (err) {
-			return "addProduct: error";
+			console.log("addProduct: error");
+			return err;
 		}
 	}
 
@@ -88,12 +89,14 @@ class ProductManager {
 		// En caso de error devolver un mensaje que diga: “getProducts: error”
 		try {
 			if (this.products.length === 0) {
-				return console.log("Not found");
+				console.log("Not found");
+				throw new Error();
 			}
 			quantity = quantity ?? 5;
 			return console.log(this.products.slice(0, quantity));
 		} catch (error) {
-			return console.log("getProducts: error");
+			console.log("getProducts: error");
+			return;
 		}
 	}
 
@@ -105,13 +108,14 @@ class ProductManager {
 			const product = this.products.find((product) => product.id === id);
 
 			if (product) {
-				console.log(product)
+				console.log(product);
 				return product;
 			} else {
-				return console.log("Not found");
+				throw new Error("product not found");
 			}
 		} catch (error) {
-			return console.log("getProductById: error");
+			console.log(`getProductById: ${error}`);
+			return error;
 		}
 	}
 
@@ -124,7 +128,8 @@ class ProductManager {
 			let product = await this.getProductById(id);
 
 			if (Object.keys(data).length === 0) {
-				return console.log("No data to update");
+				console.log("No data to update");
+				return;
 			}
 
 			for (const prop in data) {
@@ -135,10 +140,11 @@ class ProductManager {
 				this.path,
 				JSON.stringify(this.products, null, 2)
 			);
-
-			return console.log("updateProduct: done");
+			console.log("updateProduct: done");
+			return;
 		} catch (error) {
-			return console.log("updateProduct: error");
+			console.log("updateProduct: error");
+			return;
 		}
 	}
 
@@ -151,18 +157,21 @@ class ProductManager {
 			let product = await this.getProductById(id);
 
 			if (!product) {
-				return console.log("Not found");
+				console.log("Not found");
+				throw new Error();
 			}
+
 			this.products = this.products.filter((product) => product.id !== id);
 
 			await fs.promises.writeFile(
 				this.path,
 				JSON.stringify(this.products, null, 2)
 			);
-			
-			return console.log("deleteProduct: done");
+			console.log("deleteProduct: done");
+			return;
 		} catch (error) {
-			return console.log("deleteProduct: error");
+			console.log("deleteProduct: error");
+			return;
 		}
 	}
 }
@@ -173,11 +182,11 @@ let product_manager = new ProductManager("./data/products.json");
 
 /////////////////////////////
 // ADD PRODUCTS
-console.log("PRODUCTS ADDED TO products.json FILE");
-/* 
+// console.log("PRODUCTS ADDED TO products.json FILE");
 product_manager.addProduct({
 	title: "Echeveria Succulent",
-	description: "Beautiful pink and green rosette-shaped succulent. Easy to care for and perfect for any indoor or outdoor space.",
+	description:
+		"Beautiful pink and green rosette-shaped succulent. Easy to care for and perfect for any indoor or outdoor space.",
 	price: 15,
 	thumbnail: "https://example.com/echeveria.jpg",
 	stock: 8,
@@ -185,7 +194,8 @@ product_manager.addProduct({
 
 product_manager.addProduct({
 	title: "Snake Plant",
-	description: "Stylish and low-maintenance, the snake plant is perfect for any home. Its long, upright leaves add a modern touch to any room.",
+	description:
+		"Stylish and low-maintenance, the snake plant is perfect for any home. Its long, upright leaves add a modern touch to any room.",
 	price: 20,
 	thumbnail: "https://example.com/snake_plant.jpg",
 	stock: 10,
@@ -193,7 +203,8 @@ product_manager.addProduct({
 
 product_manager.addProduct({
 	title: "ZZ Plant",
-	description: "The ZZ plant is a low-light champion, making it perfect for offices and homes alike. Its glossy leaves add a touch of sophistication to any space.",
+	description:
+		"The ZZ plant is a low-light champion, making it perfect for offices and homes alike. Its glossy leaves add a touch of sophistication to any space.",
 	price: 18,
 	thumbnail: "https://example.com/zz_plant.jpg",
 	stock: 5,
@@ -201,14 +212,16 @@ product_manager.addProduct({
 
 product_manager.addProduct({
 	title: "Mint Herb Plant",
-	description: "Fresh and fragrant, this herb is perfect for adding to drinks, desserts, or as a garnish. Easy to grow and perfect for any kitchen.",
+	description:
+		"Fresh and fragrant, this herb is perfect for adding to drinks, desserts, or as a garnish. Easy to grow and perfect for any kitchen.",
 	price: 12,
 	thumbnail: "https://example.com/mint_plant.jpg",
 });
 
 product_manager.addProduct({
 	title: "Fiddle Leaf Fig",
-	description: "A popular indoor tree with large, violin-shaped leaves. Its dramatic silhouette adds a touch of elegance to any room.",
+	description:
+		"A popular indoor tree with large, violin-shaped leaves. Its dramatic silhouette adds a touch of elegance to any room.",
 	price: 45,
 	thumbnail: "https://example.com/fiddle_leaf_fig.jpg",
 	stock: 2,
@@ -216,7 +229,8 @@ product_manager.addProduct({
 
 product_manager.addProduct({
 	title: "Aloe Vera Plant",
-	description: "This succulent is not only easy to care for, but its gel can also soothe burns and other skin irritations. A must-have for any home.",
+	description:
+		"This succulent is not only easy to care for, but its gel can also soothe burns and other skin irritations. A must-have for any home.",
 	price: 17,
 	thumbnail: "https://example.com/aloe_vera.jpg",
 	stock: 6,
@@ -224,14 +238,16 @@ product_manager.addProduct({
 
 product_manager.addProduct({
 	title: "Lavender Plant",
-	description: "Known for its calming aroma, lavender is a versatile herb that can be used in cooking or for aromatherapy. Easy to care for and beautiful to look at.",
+	description:
+		"Known for its calming aroma, lavender is a versatile herb that can be used in cooking or for aromatherapy. Easy to care for and beautiful to look at.",
 	price: 14,
 	thumbnail: "https://example.com/lavender_plant.jpg",
 });
 
 product_manager.addProduct({
 	title: "Spider Plant",
-	description: "One of the easiest plants to care for, the spider plant is perfect for beginners. Its long, spindly leaves make it a great addition to any room.",
+	description:
+		"One of the easiest plants to care for, the spider plant is perfect for beginners. Its long, spindly leaves make it a great addition to any room.",
 	price: 10,
 	thumbnail: "https://example.com/spider_plant.jpg",
 	stock: 7,
@@ -239,7 +255,8 @@ product_manager.addProduct({
 
 product_manager.addProduct({
 	title: "String of Pearls",
-	description: "This unique succulent has long, thin stems with tiny green 'pearls' that trail down. Perfect for hanging baskets or as a statement piece.",
+	description:
+		"This unique succulent has long, thin stems with tiny green 'pearls' that trail down. Perfect for hanging baskets or as a statement piece.",
 	price: 22,
 	thumbnail: "https://example.com/string_of_pearls.jpg",
 	stock: 4,
@@ -247,20 +264,21 @@ product_manager.addProduct({
 
 product_manager.addProduct({
 	title: "Basil Herb Plant",
-	description: "A staple herb in many kitchens, basil is easy to grow and adds a fresh, aromatic flavor to any dish.",
+	description:
+		"A staple herb in many kitchens, basil is easy to grow and adds a fresh, aromatic flavor to any dish.",
 	price: 9,
 	thumbnail: "https://example.com/basil_plant.jpg",
 });
-*/
 
 /////////////////////////////
 // GET PRODUCT BY ID
-console.log("GET PRODUCT BY ID");
+// console.log("GET PRODUCT BY ID");
 // product_manager.getProductById(1);
 
 /////////////////////////////
 // UPDATE PRODUCT
-console.log("UPDATE PRODUCT");
+// console.log("UPDATE PRODUCT");
+// product_manager.updateProduct(9, {});
 // product_manager.updateProduct(9, {
 //  "title": "Thread of Silofornias",
 // "description": "This unique succulent has long, thin stems with tiny green 'pearls' that trail down. Perfect for hanging baskets or as a statement piece.",
@@ -271,11 +289,11 @@ console.log("UPDATE PRODUCT");
 
 /////////////////////////////
 // DELETE PRODUCT
-console.log("DELETE PRODUCT");
+// console.log("DELETE PRODUCT");
 // product_manager.deleteProduct(10);
 
 /////////////////////////////
 // GET PRODUCTS
-console.log("GET PRODUCTS");
+// console.log("GET PRODUCTS");
 // Insert quantity, default 5
-product_manager.getProducts();
+// product_manager.getProducts();
