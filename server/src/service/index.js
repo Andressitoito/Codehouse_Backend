514f7733113328bc079be96b54763b796fb2ca1a
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2023-07-21 23:54:13 -0300

    Create Products Repositories and Services Index for Server Refactoring
    Established 'products' repositories and services index as part of the ongoing server refactoring effort. This step aims to enhance code organization and maintainability by centralizing product-related data access and business logic.

--- server/src/service/index.js
+++ server/src/service/index.js
@@ -1,5 +1,12 @@
 import ProductsDaoMongo from "../dao/mongo/products/products.dao.mongo.js";
+import { ProductRepository } from "../repositories/products.repositories.js";
 
 export const productMongoService = new ProductsDaoMongo()
 // const productService = new ProductsDaoMongo() user
 // const productService = new ProductsDaoMongo() cart
+
+const productsSerice = new ProductRepository(new ProductsDaoMongo())
+
+module.exports = {
+ productsService
+}
