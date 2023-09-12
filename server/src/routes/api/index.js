57a00763d80908620413fe40bc996f7373691592
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2023-09-12 08:51:47 -0300

    Test Configuration and Successful Testing Implementation
    Configured the necessary files for running tests and successfully conducted testing procedures. All tests are functioning as expected, validating the proper functionality of the codebase.

--- server/src/routes/api/index.js
+++ server/src/routes/api/index.js
@@ -8,16 +8,16 @@ import auth_router from "./auth/auth.js";
 import ProductsMongoRouter from "./products/products.routerClass.js";
 import CartMongoRouter from "./carts/carts.routerClass.js";
 
-import productsRouter from "./products/products.mongo.js"
-import cartRouter from "./carts/carts.mongo.js"
+import productsRouter from "./products/products.mongo.js";
+import cartRouter from "./carts/carts.mongo.js";
 
 /////////////////////////////
 // VARIABLES
 /////////////////////////////
-const router = Router()
+const router = Router();
 
 const productsMongoRouter = new ProductsMongoRouter();
-const cartMongoRouter = new CartMongoRouter()
+const cartMongoRouter = new CartMongoRouter();
 
 /////////////////////////////
 // ROUTER USE
@@ -25,14 +25,13 @@ const cartMongoRouter = new CartMongoRouter()
 // router.use('/products_mongo', productsMongoRouter.init)
 // router.use('/carts_mongo', cartMongoRouter.init)
 
-router.use('/products_mongo', productsRouter)
-router.use('/carts_mongo', cartRouter)
-router.use('/cookies', cookies_router)
-router.use('/sessions', sessions_router)
-router.use('/auth', auth_router)
+router.use("/products_mongo", productsRouter);
+router.use("/carts_mongo", cartRouter);
+router.use("/cookies", cookies_router);
+router.use("/sessions", sessions_router);
+router.use("/auth", auth_router);
 
 /////////////////////////////
 // EXPORT ROUTER
 /////////////////////////////
-export default router
-
+export default router;
--- server/test/ChaiProduct.test.js
+++ server/test/ChaiProduct.test.js
@@ -4,28 +4,22 @@ import Assert from "assert";
 
 mongoose.connect(process.env.MONGO_LINK);
 
-const expect = chai.expect
-
+const expect = chai.expect;
 
 describe("Testing Product Dao", () => {
- before(function () {
-  this.productsMongoDao = new Products();
- });
+	before(function () {
+		this.productsMongoDao = new Products();
+	});
 
- beforeEach(function () {
-  this.timeout(2000);
- });
- 
- it('Our dao must get all products', async function () {
-  const result = await this.productsMongoDao.get()
+	beforeEach(function () {
+		this.timeout(2000);
+	});
 
-  // expect(result).to.be.deep.equal([])
+	it("Our dao must get all products", async function () {
+		const result = await this.productsMongoDao.get();
 
-  expect(Array.isArray(result)).to.be.ok
+		// expect(result).to.be.deep.equal([])
 
-
- })
-})
-
-
-
+		expect(Array.isArray(result)).to.be.ok;
+	});
+});
